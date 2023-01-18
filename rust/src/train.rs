1a931e75e3a33465d31e59fe2453bce3cf1171a8
SpikeHD <spikegdofficial@gmail.com>
2023-01-18 01:53:00 -0800

    attempt image capture

--- rust/src/train.rs
+++ rust/src/train.rs
@@ -1,6 +1,6 @@
 use std::{fs, path::PathBuf, time::Instant};
-use screenshots::Screen;
-
+use scrap;
+use image;
 use quad_gamepad::*;
 
 //These go by Guitar Hero guitar button color, nto button name. Easier to keep track of
@@ -21,10 +21,15 @@ pub fn capture_data() {
   let mut btn_map: Vec<(String, u128)> = Vec::new();
   let begin_ts = std::time::Instant::now();
 
+  // For getting screenshots
+  let mut capturer = get_capturer();
+  let w = capturer.width();
+  let h = capturer.height();
+
   // When a button is pressed, take a screenshot and record which buttons are being pressed
   // Keep track of previous state to see if we have been holding a button
   loop {
-    std::thread::sleep(std::time::Duration::from_millis(32));
+    std::thread::sleep(std::time::Duration::from_millis(46));
 
     controller.update();
     itr += 1;
@@ -59,13 +64,47 @@ pub fn capture_data() {
     }
     println!("");
 
-    // Get screen
-    let screens = Screen::all().unwrap();
-    let screen = screens.first().unwrap();
+    let ts = begin_ts.elapsed().as_millis();
+
+    // Capture frame and write
+    match capturer.frame() {
+      Ok(frame) => {
+        let buffer = &*frame;
+        let mut bitflipped = Vec::with_capacity(w * h * 4);
+        let stride = buffer.len() / h;
+
+        for y in 0..h {
+          for x in 0..w {
+            let i = stride * y + 4 * x;
+            bitflipped.extend_from_slice(&[
+              buffer[i],
+              buffer[i + 1],
+              buffer[i + 2],
+          ]);
+        }
+
+        // Write bitflipped image
+        image::save_buffer_with_format(
+          PathBuf::from(format!("./training_images/{}.png", ts)),
+          &bitflipped,
+          w as u32,
+          h as u32,
+          image::ColorType::Rgba8,
+          image::ImageFormat::Png
+        );
+      } 
+      }
+      Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+        // Wait.
+      },
+      Err(e) => {
+        println!("{}", e);
+      }
+    };
 
     if btn_state.len() > 0 {
-      // Write buf to filesystem
-      let pair = (btn_string, begin_ts.elapsed().as_millis());
+      // Store timestamp/button combo
+      let pair = (btn_string, ts);
       btn_map.push(pair);
     }
 
@@ -76,4 +115,9 @@ pub fn capture_data() {
       break;
     }
   }
+}
+
+pub fn get_capturer() -> scrap::Capturer {
+  let display = scrap::Display::primary().unwrap();
+  scrap::Capturer::new(display).unwrap()
 }
