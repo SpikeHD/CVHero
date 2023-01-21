3c5020eea5d56d7695c9336a8fd0e8e8d97f349e
SpikeHD <spikegdofficial@gmail.com>
2023-01-21 01:10:19 -0800

    more tweaking (really good now)

--- main.cpp
+++ main.cpp
@@ -13,27 +13,161 @@
 using namespace std;
 
 // Y value to hit buttons at
-static int MIN_Y = 600;
+static int MIN_Y = 70;
 
 // Button pressing thresholds
-static int A_Min = 1000;
-static int A_Max = 1080;
-static int B_Min = 1100;
-static int B_Max = 1180;
-static int Y_Min = 1200;
-static int Y_Max = 1280;
-static int X_Min = 1300;
-static int X_Max = 1380;
-static int L_Min = 1400;
-static int L_Max = 1480;
+static int A_Min = 50;
+static int A_Max = 90;
+static int B_Min = 140;
+static int B_Max = 180;
+static int Y_Min = 230;
+static int Y_Max = 260;
+static int X_Min = 300;
+static int X_Max = 340;
+static int L_Min = 380;
+static int L_Max = 430;
+
+// DEBUG
+static bool controlDisabled = false;
 
 int curTimeMs() {
   auto duration = std::chrono::system_clock::now().time_since_epoch();
   return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
 }
 
+bool doNotePresses(int &delayEnd, cv::Point p) {
+  bool btnsHeld = false;
+
+  // Check thresholds and see if we can press a button
+  // Order is ABYXL
+  bool delayed = delayEnd > curTimeMs();
+  if (p.y > MIN_Y && !controlDisabled && !delayed) {
+    if (p.x > A_Min && p.x < A_Max) {
+      pressKey(SL::Input_Lite::KEY_A);
+      btnsHeld = true;
+    }
+
+    if (p.x > B_Min && p.x < B_Max) {
+      pressKey(SL::Input_Lite::KEY_S);
+      btnsHeld = true;
+    }
+
+    if (p.x > Y_Min && p.x < Y_Max) {
+      pressKey(SL::Input_Lite::KEY_J);
+      btnsHeld = true;
+    }
+
+    if (p.x > X_Min && p.x < X_Max) {
+      pressKey(SL::Input_Lite::KEY_K);
+      btnsHeld = true;
+    }
+
+    if (p.x > L_Min && p.x < L_Max) {
+      pressKey(SL::Input_Lite::KEY_L);
+      btnsHeld = true;
+    }
+  }
+
+  return btnsHeld;
+}
+
+bool doOpenPress(int &delayEnd, cv::Point p) {
+  bool pressed = false;
+  bool delayed = delayEnd > curTimeMs();
+
+  if (p.y > MIN_Y && !controlDisabled && !delayed) {
+    // Hitting open notes requires not having anything pressed
+    unpressAll();
+    strum();
+
+    pressed = true;
+  }
+
+  return pressed;
+}
+
+void handleTplMatch(cv::Mat &img, cv::Mat &tpl, double thresholdMin, int &delayEnd, string tplType) {
+  cv::Size s;
+
+  // Note size
+  s.height = img.rows - tpl.rows + 1;
+  s.width = img.cols - tpl.cols + 1;
+
+  auto zero = cv::Mat::zeros(
+    s,
+    CV_32SC1
+  );
+
+  auto result = cv::Mat();
+
+  cv::matchTemplate(img, tpl, result, cv::TM_CCORR_NORMED);
+
+  auto threshold = cv::Mat();
+
+  cv::threshold(result, threshold, thresholdMin, 1.0, cv::THRESH_BINARY);
+
+  auto pts = vector<cv::Point>();
+
+  // Filter out non-zeros
+  cv::findNonZero(threshold, pts);
+
+  // eep trck of whether any amount of buttons is being held
+  bool btnsHeld = false;
+
+  if (pts.size() > 0) {
+    bool firstPt = true;
+    auto lastPt = pts.at(0);
+
+    for (cv::Point p : pts) {
+      // If the last found point is close enough to this one, just skip it
+      if (!firstPt && abs(p.x - lastPt.x) < 50 && abs(p.y - lastPt.y) < 30) continue;
+
+      // More strictness on open notes
+      if (tplType == "open") {
+        if (abs(p.x - lastPt.x) < 70 && abs(p.y - lastPt.y) < 30) continue;
+      }
+
+      firstPt = false;
+      lastPt = p;
+      
+      auto rec = cv::Rect();
+      rec.x = p.x;
+      rec.y = p.y;
+      rec.width = tpl.cols;
+      rec.height = tpl.rows;
+
+      auto col = cv::Scalar(255, 255, 255);
+      cv::rectangle(img, rec, col);
+
+      // DEBUG show coordinates of match
+      stringstream s;
+      s << "X: " << p.x << "Y: " << p.y;
+
+      cv::putText(
+        img,
+        s.str(),
+        p,
+        cv::FONT_HERSHEY_COMPLEX_SMALL,
+        1.0,
+        col
+      );
+
+      if (tplType == "note") btnsHeld = doNotePresses(delayEnd, p);
+      if (tplType == "open") btnsHeld = doOpenPress(delayEnd, p);
+    }
+
+    if (btnsHeld) {
+      delayEnd = curTimeMs() + chrono::milliseconds(45).count();
+
+      strum();
+      unpressAll();
+    }
+  }
+}
+
 int main() {
   auto noteTpl = cv::imread("./notes_images/note_greyscale_small.png", cv::ImreadModes::IMREAD_GRAYSCALE);
+  auto barTpl = cv::imread("./notes_images/note_open_greyscale_diff.png", cv::ImreadModes::IMREAD_GRAYSCALE);
 
   cv::namedWindow("window", cv::WINDOW_NORMAL);
 
@@ -50,107 +184,11 @@ int main() {
     auto img = cv::Mat(height, width, bpp > 24 ? CV_8UC4 : CV_8UC3, &pixels[0]);
     cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
 
-    cv::Size s;
+    // Crop to make processing a bit better
+    img = img(cv::Range(550, 700), cv::Range(1000, 1550));
 
-    s.height = img.rows - noteTpl.rows + 1;
-    s.width = img.cols - noteTpl.cols + 1;
-    auto zero = cv::Mat::zeros(
-      s,
-      CV_32SC1
-    );
-
-    auto result = cv::Mat();
-
-    cv::matchTemplate(img, noteTpl, result, cv::TM_CCORR_NORMED);
-
-    auto threshold = cv::Mat();
-
-    cv::threshold(result, threshold, 0.77, 1.0, cv::THRESH_BINARY);
-
-    auto pts = vector<cv::Point>();
-
-    // Filter out non-zeros
-    cv::findNonZero(threshold, pts);
-
-    auto noteTplSize = noteTpl.size();
-
-    // eep trck of whether any amount of buttons is being held
-    bool btnsHeld = false;
-
-    if (pts.size() > 0) {
-      bool firstPt = true;
-      auto lastPt = pts.at(0);
-
-      for (cv::Point p : pts) {
-        // If the last found point is close enough to this one, just skip it
-        if (!firstPt && abs(p.x - lastPt.x) < 30 && abs(p.y - lastPt.y) < 30) continue;
-
-        firstPt = false;
-        lastPt = p;
-        
-        auto rec = cv::Rect();
-        rec.x = p.x;
-        rec.y = p.y;
-        rec.width = noteTpl.cols;
-        rec.height = noteTpl.rows;
-
-        auto col = cv::Scalar(255, 255, 255);
-
-        cv::rectangle(img, rec, col);
-
-        // DEBUG show coordinates of match
-        stringstream s;
-        s << "X: " << p.x << "Y: " << p.y;
-
-        cv::putText(
-          img,
-          s.str(),
-          p,
-          cv::FONT_HERSHEY_COMPLEX_SMALL,
-          1.0,
-          col
-        );
-
-        // Check thresholds and see if we can press a button
-        // Order is ABYXL
-        bool controlDisabled = false;
-        bool delayed = delayEnd > curTimeMs();
-
-        if (p.y > MIN_Y && !controlDisabled && !delayed) {
-          if (p.x > A_Min && p.x < A_Max) {
-            pressKey(SL::Input_Lite::KEY_A);
-            btnsHeld = true;
-          }
-
-          if (p.x > B_Min && p.x < B_Max) {
-            pressKey(SL::Input_Lite::KEY_S);
-            btnsHeld = true;
-          }
-
-          if (p.x > Y_Min && p.x < Y_Max) {
-            pressKey(SL::Input_Lite::KEY_J);
-            btnsHeld = true;
-          }
-
-          if (p.x > X_Min && p.x < X_Max) {
-            pressKey(SL::Input_Lite::KEY_K);
-            btnsHeld = true;
-          }
-
-          if (p.x > L_Min && p.x < L_Max) {
-            pressKey(SL::Input_Lite::KEY_L);
-            btnsHeld = true;
-          }
-        }
-      }
-    }
-
-    if (btnsHeld) {
-      delayEnd = curTimeMs() + chrono::milliseconds(70).count();
-
-      strum();
-      unpressAll();
-    }
+    handleTplMatch(img, noteTpl, 0.77, delayEnd, "note");
+    handleTplMatch(img, barTpl, 0.91, delayEnd, "open");
 
     cv::resize(img, img, cv::Size {
       852, 480
